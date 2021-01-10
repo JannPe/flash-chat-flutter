fd5e412eecd2069a99fb7de8f4c576efd2340a8e
Jann <jann.peters@web.de>
2021-01-10 15:52:39 +0100

    Flash chat navigation added and refactored

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,27 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+
+import 'screens/login_screen.dart';
+
+import 'screens/registration_screen.dart';
+
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.route,
+      routes: {
+        WelcomeScreen.route: (context) => WelcomeScreen(),
+        LoginScreen.route: (context) => LoginScreen(),
+        RegistrationScreen.route: (context) => RegistrationScreen(),
+        ChatScreen.route: (context) => ChatScreen(),
+      },
     );
   }
 }
